e2b818e4e002fa4d7d51c7be58cbafe9c7db3295
Casey Wilms <casey@bikoo.jp>
2012-10-01 16:02:52 -0700

    Changes to single.php formatting
    Changes to single.php formatting and css for h2 headings in posts.

--- wp-content/themes/bootstrapwp-87/single.php
+++ wp-content/themes/bootstrapwp-87/single.php
@@ -15,24 +15,24 @@ get_header(); ?>
 <?php while ( have_posts() ) : the_post(); ?>
    <div class="container-fluid">
      <div class="row-fluid">
-        <div class="row content">
-          <div class="span4">
+          <div class="span5">
+          	<div class="post-image">
               <?php getAttachedimages('large', '<div class="post-image-holder">', '</div>'); ?>
-          </div><!-- /.span4 -->
-<div class="span8">
-
-     
+              </div>
+          </div><!-- /.span5 -->
+		  <div class="span7">
+        	<div class="row content">
  <!-- Masthead
       ================================================== -->
-      <header class="jumbotron subhead" id="overview">
-        <h1><?php the_title();?></h1>
-      </header>
-   <p class="meta"><?php echo bootstrapwp_posted_on();?></p>
-            <?php the_content();?>
-            <?php the_tags( '<p>Tags: ', ', ', '</p>'); ?>
-<?php endwhile; // end of the loop. ?>
-<hr />
-          </div><!-- /.span8 -->
-          </div><!-- /.row -->
-          </div><!-- /.container -->
+     		  <header class="jumbotron subhead" id="overview">
+     		    <h2><?php the_title();?></h2>
+     		  </header>
+     		  <?php the_content();?>
+     		  <?php the_tags( '<p>Tags: ', ', ', '</p>'); ?>
+     		  <?php endwhile; // end of the loop. ?>
+     		  <hr />
+     		</div>
+          </div><!-- /.span7 -->
+        </div><!-- /.row -->
+     </div><!-- /.container -->
 <?php get_footer(); ?>
