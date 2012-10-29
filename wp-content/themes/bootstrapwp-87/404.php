e6147e0d1ccf4501983fc0dccf291104edb3724f
Casey Wilms <casey@bikoo.jp>
2012-10-29 14:08:02 -0700

    Completed Custom 404 Page
    Added formatting, first chapter, and product links to the custom 404
    page.

--- wp-content/themes/bootstrapwp-87/404.php
+++ wp-content/themes/bootstrapwp-87/404.php
@@ -20,35 +20,173 @@ get_header(); ?>
  <!-- Masthead
       ================================================== -->
       <header class="jumbotron subhead" id="overview">
-        <h1><?php _e( 'This is Embarrassing', 'bootstrapwp' ); ?></h1>
-        <p class="lead"><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching, or one of the links below, can help.', 'bootstrapwp' ); ?></p>
+        <h1><?php _e( 'Oh no!', 'bootstrapwp' ); ?></h1>
+        <p class="lead"><?php _e( 'Bikoo cannot find the page you are looking for!  For now, why not read the first chapter of Demon Hunters?', 'bootstrapwp' ); ?></p>
       </header>
-	  
-        <div class="row content">
-<div class="span8">
-					
+	</div>
+	
+<div class="container-fluid">
+  <div class="row-fluid">
+    <div class="span9">
+      <a target="_blank" href="http://www.amazon.com/gp/product/B009SJ3JPM/ref=as_li_tf_il?ie=UTF8&amp;camp=1789&amp;creative=9325&amp;creativeASIN=B009SJ3JPM&amp;linkCode=as2&amp;tag=bikoo-20"><img src="http://bikoo.jp/wp-content/uploads/bikoo/404Page_Demons.jpg" width="700"></a>
+    <div id="content_10-15.html" xml:lang="en-US" xmlns:xml="http://www.w3.org/XML/1998/namespace">
+    <div class="story">
+      <p class="Chapter">Chapter 1</p>
 
-<div class="well">
-					<?php get_search_form(); ?>
+      <p class="Subheading" id="toc_marker-3-1">The Dark Ritual of Female Flesh</p>
 
-</div><!--/.well -->
-<div class="row">
-<div class="span4">
-					<h2>All Pages</h2>
-					<?php wp_page_menu(); ?>
-</div><!--/.span4 -->
-<div class="span4">
-					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
+      <p class="SubChapter">1</p>
 
-					
-						<h2><?php _e( 'Most Used Categories', 'bootstrapwp' ); ?></h2>
-						<ul>
-						<?php wp_list_categories( array( 'orderby' => 'count', 'order' => 'DESC', 'show_count' => 1, 'title_li' => '', 'number' => 10 ) ); ?>
-						</ul>
-					
-</div><!--/.span4 -->
-</div><!--/.row -->
-					</div><!--/.span8 -->
- <?php get_sidebar(); ?>
+      <p class="FirstLine">Wind howled through the darkness.</p>
 
+      <p class="Main-Text">There was something ominous in the way it swept up the valley slope, manifesting from the black void. The undergrowth of the copse rustled in its wake creating a constant whisper.</p>
+
+      <p class="Main-Text">Fuminari Senkichi scowled at the stench of damp grass. The flora of the valley had absorbed the day&#39;s heat, and now it was being released into the night, tinged with the essence of the plants. The stench was raw, not unlike blood. Fuminari knew it well; he felt no particular aversion to it.</p>
+
+      <p class="Main-Text"><span class="italics">Mmm...</span></p>
+
+      <p class="Main-Text">In the dark, Fuminari&#39;s expression shifted into something of a wry smile. The corners of his mouth traced upward in a subtle curve--extremely rare for him. He had been known to grin subconsciously in the middle of a fight, even when there was no apparent reason. There was just something in him, a brutishness, that thrived on tension.</p>
+
+      <p class="Main-Text">Now sitting motionless, he glanced beyond the canopy of trees toward the pale-white, star-pricked sky like a predator concealed in the undergrowth. The bluish ellipse of a twisted half-moon hung dimly above. He sat on a hill cast in darkness, the thickly forested valley beyond resembling a huge, dark cave.</p>
+
+      <p class="Main-Text">He was almost naked, only an open shirt covered his bare skin. The woman in his arms, Kumiko Nakano, was in a similar state of undress. She was like a child in Fuminari&#39;s embrace, curled up between his crossed legs as they sat in the grass. Of course, far from being a child, she was a fully developed woman, but she appeared tiny next to the extraordinary bulk of the man holding her.</p>
+
+      <p class="Main-Text">They were in Nishitanzawa. It was a summer night, but the air at a thousand meters was cold, too cold to be cavorting about without any clothes. Despite this, the woman&#39;s skin was moistened by a thin layer of sweat. Fuminari took her taut, heavy breasts in his hands and scooped them upward. He massaged her nipples and kissed her. Their tongues met, mixing saliva. Satisfied, Fuminari took her hardened nipples into his mouth, each was the size of an infant&#39;s finger; he sucked at them, playing his tongue over them as they became even harder, redder.</p>
+
+      <p class="Main-Text">&#34;I wonder if they&#39;ll show,&#34; Kumiko spoke in a voice so light it was almost inaudible. Fuminari pulled his mouth away, continuing to tweak her nipples with his fingers.</p>
+
+      <p class="Main-Text">&#34;They&#39;ll be here,&#34; he moved closer and talked into her ear, nibbling her earlobe as he continued to tease his fingertips over her hard, moistened nipples.</p>
+
+      <p class="Main-Text">&#34;They&#39;ll know it&#39;s a trap...&#34;</p>
+
+      <p class="Main-Text">&#34;And they&#39;ll know this is gonna be their last chance,&#34; as he whispered his response, Fuminari moved his lips down, tracing a line from her ear to the nape of her neck--her fine hairs stood on end. &#34;&#34;You fucked Muto, huh?&#34;</p>
+
+      <p class="Main-Text">&#34;You knew?&#34;</p>
+
+      <p class="Main-Text">&#34;I could see it in his eyes.&#34;</p>
+
+      <p class="Main-Text">&#34;It was before I met you, just once.&#34;</p>
+
+      <p class="Main-Text">&#34;He&#39;s in love.&#34;</p>
+
+      <p class="Main-Text">Kumiko was silent.</p>
+
+      <p class="Main-Text">&#34;He&#39;ll be the first to attack.&#34; Fuminari moved his mouth back to Kumiko&#39;s ear. He spoke softly, &#34;definitely Muto,&#34; at the same time reaching down and sliding his hand up the inside of her thighs. She moaned softly; her voice was sweet, already giving into pleasure. Her body felt hot and flush, ripe; she began to sway. Fuminari&#39;s hands teased, tracing lines around the top of her thighs. He swept them around her back, bringing them down her waist to her buttocks. His fingernails scratched gently along the familiar lines of her body. Thick, rugged fingers danced subtle steps across her skin.</p>
+
+      <p class="Main-Text">&#34;Stop...stop... &nbsp;You&#39;ll make me horny.&#34; Kumiko tightened her grip around Fuminari&#39;s neck and arched her back, trying to pull away. Fuminari grabbed her firmly, dragging her back toward him. He slid his fingers along the line of her buttocks and pushed one into her. It felt like puncturing a boiled tomato. His finger moved through hot, sticky fluid as it traced out her swollen clitoris. Kumiko swung her hips into him, rubbing herself against Fuminari&#39;s finger.</p>
+
+      <p class="Main-Text">&#34;Mmmm.&#34; Her breathing had become rough and heavy.</p>
+
+      <p class="Main-Text">&#34;That&#39;s more like it.&#34; Fuminari lowered her to the grass and crouched, keeping an arm under her, thinking it would be a shame for her light skin to get scratched up. She wrapped her legs around his thighs, locking the two of them together. She reached out and grabbed his erection, guiding it toward the hot, splayed out center of her legs. She brushed the tip over her moist opening, relishing the sensation. He was hot and swollen; his dick felt coiled, ready to break free if she loosened her grip even slightly.</p>
+
+      <p class="Main-Text"><span class="italics">What a great accomplice,</span> Fuminari thought, <span class="italics">if the situation were different I&#39;d take my time, build her up...then fuck her until her eyes rolled back.</span></p>
+
+      <p class="Main-Text">He turned her over, face up, spread her pale legs wide, then stuck his cock deep inside her. Kumiko&#39;s hips swung upward as she wrapped her legs eagerly around him. As they sank into the undergrowth together, the sound of the river flowing through the base of the dark valley was replaced by the noisy rustling of leaves.</p>
+
+      <p class="Main-Text">Fuminari began to thrust. He had been taken by a strangely violent mood--he wanted to fuck Kumiko hard, to spoil her pure skin with the green sap of the grass. So he took her without restraint, forcing her legs wide, pivoting and twisting her as though he harbored a resentment of the female body. Still inside her, he brought a finger down to her anus; she almost choked. He stuck the finger in to the first knuckle. Kumiko was fast loosing the ability to speak coherently.</p>
+
+      <p class="Main-Text">Fuminari&#39;s thrusting picked up speed; Kumiko ground her pinned-down hips into him out of sheer delirium, her juices cascading as he penetrated her. She grabbed at her breasts, writhing under him with an intensity like she were trying to break free--her head jerked back and forth, her stunning features distorted. The pinkness of her tongue danced with a life of its own in her open mouth as though seeking the source of pleasure in the air. She let out a honeyed moan that was like a physical representation of her joy. At that moment, Fuminari leaned forward and whispered into her ear, &#34;They&#39;re coming. Up there.&#34;</p>
+
+      <p class="Main-Text">A shadow moved upslope to Fuminari&#39;s left, surprisingly close. It roared. A thin iron pipe smashed into the ground next to them. Fuminari leaped into the air, away from Kumiko&#39;s side, somersaulting once before rolling into nearby cover. The shadow followed, a series of attacks rained down in succession. Fuminari pitched and rolled, deftly avoiding them. The shadow drew back. It was crouched low and ready, just a few meters away in the darkness--only the sound of heavy breathing permeated the space between them. The attacks had spanned just a few seconds, but the intensity must have drained huge reserves of strength.</p>
+
+      <p class="Main-Text">Fuminari drew himself to his feet. He was unbelievably huge, almost impossibly so considering the acrobatics of a moment ago, towering at almost two meters. He was rugged like rock, but his frame was superbly balanced. His features were set deep above a thick neck, charming if he smiled, but at the same time, they hinted at something powerful, demonic. For now, he kept his expression neutral. Even just standing there his massive frame exuded an almost physical force into the air.</p>
+
+      <p class="Main-Text">&#34;Muto, right?&#34; Fuminari called out to the shadow. His breathing was perfectly measured.</p>
+
+      <p class="Main-Text">&#34;Fell right for it.&#34; Kumiko was up; a grin played across her red lips as she spoke. She was holding a collapsible shovel.</p>
+
+      <p class="Main-Text">&#34;Kawaguchi, get out here,&#34; Fuminari shouted into the darkness behind him, his gaze fixed on Muto.</p>
+
+      <p class="Main-Text">There was a rustling behind them. Kumiko sprang to Fuminari&#39;s side with panther-like agility, but nothing attacked. The rustling circled around the darkness to their side and a second shadow appeared next to Muto.</p>
+
+      <p class="Main-Text">&#34;Traitorous bastard!&#34; the shadow spat the words in anger. The venom was almost palpable as it cut through the night; a weaker opponent might have been forced to look away.</p>
+
+      <p class="Main-Text">&#34;Kawaguchi, as suspected,&#34; Fuminari said in flat monotone. He stood with his legs slightly apart, empty hands hanging casually at his sides. He made no attempt to hide himself--his erection was still firm. At first glance, he appeared to be defenseless, yet he still managed to be intimidating. He was completely naked apart from his hiking boots and open shirt. There was something otherworldly about him; his massive body emanated latent energy like a densely coiled steel spring, ready to jump over his own height regardless of the pose. He took a generous step forward. The shadows stumbled backward, overawed.</p>
+
+      <p class="Main-Text">&#34;Where&#39;s our money?&#34; Muto shouted; his voice was tense.</p>
+
+      <p class="Main-Text">Fuminari said nothing.</p>
+
+      <p class="Main-Text">&#34;It&#39;s ours!&#34; Kawaguchi groaned.</p>
+
+      <p class="Main-Text">&#34;Hah!&#34; Fuminari shook his head softly, mockingly, &#34;I&#39;d imagine Towa Bank is saying the same thing, after all, we stole it from them.&#34;</p>
+
+      <p class="Main-Text">&#34;Shut the fuck up!&#34;</p>
+
+      <p class="Main-Text">&#34;The money&#39;s a war chest for the <span class="italics">Kokushigun</span>!&#34;</p>
+
+      <p class="Main-Text">&#34;I have no interest in your games of revolution.&#34;</p>
+
+      <p class="Main-Text">&#34;You lied to us.&#34;</p>
+
+      <p class="Main-Text">&#34;Look, it&#39;s a measly 100 million yen. You&#39;re not going to start a revolution with that, but it&#39;s enough to keep me in the high life for a while,&#34; Fuminari flooded his voice with scorn. He was trying to provoke them.</p>
+
+      <p class="Main-Text">&#34;Kumiko, did you betray Iwakura too?&#34; Muto yelled. Iwakura was the head of the <span class="italics">Kokushigun</span>.</p>
+
+      <p class="Main-Text">&#34;I like strong guys, Muto. Guys like Fuminari,&#34; she answered coolly. She made no effort to conceal herself in the darkness; apart from an open shirt, she was completely naked.</p>
+
+      <p class="Main-Text">&#34;If he hadn&#39;t been caught, Iwakura would never have let you get away with this.&#34;</p>
+
+      <p class="Main-Text">&#34;Iwakura, right.&#34; Fuminari&#39;s lips curled into a grin.</p>
+
+      <p class="Main-Text">&#34;It was you? You sold him to the cops?&#34;</p>
+
+      <p class="Main-Text">&#34;If I did?&#34;</p>
+
+      <p class="Main-Text">&#34;Fucker!&#34; Muto yelled, readying a posture for attack.</p>
+
+      <p class="Main-Text">Fuminari crouched, picking something up from his feet. He hurled it casually into the air. &#34;Here, your money...&#34; A package wrapped in a plastic bag thumped to the ground next to Muto and Kawaguchi distracting them for the briefest of moments--Fuminari launched his massive frame into the air.</p>
+
+      <p class="Main-Text">&#34;Shit!&#34; Muto swung a steel pipe at Fuminari&#39;s landing place. Fuminari leaned back, easily avoiding the attack. The tip of the pipe whooshed noisily through the air, millimeters away from his nose.</p>
+
+      <p class="Main-Text">Light flooded over the scene.</p>
+
+      <p class="Main-Text">Kumiko had switched on the torches they had brought with them. The battery-powered light was weak, but it was all Fuminari needed. He saw Muto in front of him wielding the steel pipe, Kawaguchi a mountain knife. They looked like men still in their 20&#39;s. Muto&#39;s face was wide-eyed, contorted with rage. He let out a shrill, birdlike screech; the veins across his temples were swollen, pulsing subtly. Kawaguchi was half-turned, ready to make an escape, but Kumiko had circled around behind him, lithe as a black cat. Kawaguchi rushed to face her. Fuminari and Muto, Kumiko and Kawaguchi--the two pairs squared off.</p>
+
+      <p class="Main-Text">Fuminari looked into Muto&#39;s bloodshot eyes and narrowed his own in a smile. &#34;I hear you fucked Kumiko,&#34; he whispered.</p>
+
+      <p class="Main-Text">Muto&#39;s face went red. He screamed something nonsensical and brought the steel pipe swinging down. Fuminari made his move. He slid in toward Muto&#39;s chest, casually avoiding the blow; when their faces met, they were less than 20 centimeters apart. Fuminari&#39;s mouth curled into a grin. It was the last image Muto would ever see of the world. Fuminari slammed the side of his hand into the base of Muto&#39;s neck; he crumbled wordlessly to the ground. He convulsed on the floor lying face up, eyes open and staring. Fuminari took the steel pipe. Kawaguchi, still engaged with Kumiko, noticed this and made to run. Kumiko gave chase.</p>
+
+      <p class="Main-Text">&#34;Move!&#34; Fuminari shouted.</p>
+
+      <p class="Main-Text">Kumiko ducked as Fuminari hurled the pipe. It buzzed through the air, slamming into Kawaguchi&#39;s back with a gruesome crunch. He collapsed forward and stopped moving.</p>
+
+      <p class="Main-Text">Kumiko checked that he was dead before confiscating the mountain knife. She walked over to the still-convulsing Muto and crouched over him with the knife in her right hand. She tossed him onto his stomach, straddled his back, then grabbed his hair in her other hand and jerked his head back. She brought the knife around to his throat and slit it open with a single clean motion. Blood fountained out in finger-thick spurts. It flowed out erratically, mirroring the beating of his heart before eventually subsiding to a slow trickle. A tepid circle of blood spread through the grass around the man&#39;s neck. The stench was awful.</p>
+
+      <p class="Main-Text">&#34;Impressive girl,&#34; Fuminari commented. Just a few moments earlier she had been underneath him moaning with pleasure, then she held her own in a fight with a man before personally slitting the throat of an ex-lover.</p>
+
+      <p class="Main-Text">&#34;You&#39;re upset that I wasn&#39;t killed, huh?&#34;</p>
+
+      <p class="Main-Text">&#34;Oh yeah,&#34; Kumiko walked across to the smiling Fuminari and wrapped her arms around him. Something prodded her in the stomach.</p>
+
+      <p class="Main-Text">&#34;Mmm...&#34; Kumiko smiled a lecherous smile, taking hold of his cock. It was still fully erect, burning hot. &#34;It appears that a certain something needs taking care of before we clean this up.&#34; She laid down across from Muto&#39;s lifeless body.</p>
+
+      <p class="Main-Text">As Fuminari thrust into her, his senses were assaulted by the heady stink of grass and blood. As he pumped away, it struck him just how similar the two were.</p>
+      
+      <a class="Chapter" href="http://www.amazon.com/gp/product/B009SJ3JPM/ref=as_li_tf_il?ie=UTF8&amp;camp=1789&amp;creative=9325&amp;creativeASIN=B009SJ3JPM&amp;linkCode=as2&amp;tag=bikoo-20"><h4>Read more on the Kindle Bookstore</h4></a>
+    </div>
+    </div>
+     </div>
+     <hr />
+    <div class="span3">
+      <div class="NewsWidget">
+		<div class="NewsWidget">
+      <?php
+              // Blog post query
+      query_posts( array( 'post_type' => 'post', 'category_name'=> 'news', 'showposts'=>8) );
+      if (have_posts()) : while ( have_posts() ) : the_post(); ?>
+      <div class="NewsSide">
+      	<a href="<?php the_permalink(); ?>" title="<?php the_title();?>"><h6><?php the_title();?></h6></a>
+        <?php echo shortcontent(30,40);?>
+      </div>
+      <div class="NewsTime">
+      	<p><?php echo bootstrapwp_posted_on();?></p>
+      </div>
+     <?php endwhile; endif; ?>
+    </div>
+      </div>
+    </div>
+    </div>
+  </div>
+</div><!-- /.container-fluid -->
 <?php get_footer(); ?>
