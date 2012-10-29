e0054ab7cd1cc1f1ab8e1228531582ac0e925f56
Casey Wilms <casey@bikoo.jp>
2012-10-29 11:12:32 -0700

    Fix for single.php
    Added single_blog.php and pointed single.php to the new document.

--- wp-content/themes/bootstrapwp-87/single.php
+++ wp-content/themes/bootstrapwp-87/single.php
@@ -3,7 +3,7 @@ if (in_category('4')||in_category('13')) {
 	include (TEMPLATEPATH . '/single_news.php');
 }
 elseif (in_category('35')||in_category('36')) { 
-	include (TEMPLATEPATH . '/single_project.php');
+	include (TEMPLATEPATH . '/single_blog.php');
 }
 else { 
 	include (TEMPLATEPATH . '/single_project.php');
