c40bf65ebbc86de59a8f76cea2c31b994fa9aa19
Casey Wilms <casey@bikoo.jp>
2012-10-30 11:41:43 -0700

    Added Blog Widget
    Displays 8 recent blog posts in order of date excluding the current
    post.

--- wp-content/themes/bootstrapwp-87/single_blog.php
+++ wp-content/themes/bootstrapwp-87/single_blog.php
@@ -32,12 +32,32 @@ get_header(); ?>
          <p class="meta"><?php echo bootstrapwp_posted_on();?></p>
             <?php the_content();?>
             <?php the_tags( '<p>Tags: ', ', ', '</p>'); ?>
-			<?php endwhile; // end of the loop. ?>
+			<?php endwhile; wp_reset_query(); // end of the loop reset query. ?>
 		<hr />
 		    <?php comments_template(); ?>
        </div>
+       <div class="span1"></div>
        <div class="span3">
-       
+         <div class="NewsWidget">
+           <h5>Latest Posts</h5>
+           <!--/awesome code to exclude current post from the query, delivering all posts from current category -->
+           <?php
+           global $wp_query;
+           $cat_ID = get_the_category($post->ID);
+           $cat_ID = $cat_ID[0]->cat_ID;
+           $this_post = $post->ID;
+           query_posts(array('cat' => $cat_ID, 'post__not_in' => array($this_post), 'showposts' => 8));
+           if (have_posts()) : while ( have_posts() ) : the_post(); 
+           ?>
+           <div class="NewsSide">
+             <a href="<?php the_permalink(); ?>" title="<?php the_title();?>"><h6><?php the_title();?></h6></a>
+             <?php echo shortcontent(30,80);?>
+           </div>
+           <div class="NewsTime">
+             <p><?php echo bootstrapwp_posted_on();?></p>
+           </div>
+           <?php endwhile; endif; ?>
+         </div>
        </div>
      </div>
    </div>
